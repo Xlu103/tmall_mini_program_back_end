2d0f1ebed9fac8daca53f8625c595bf36b404604
xlu103 <1534957736@qq.com>
2020-12-13 22:21:40 +0800

    订单查询，订单添加，订单删除，地址管理基本完成

--- src/main/java/com/tmall/controller/ManagerController.java
+++ src/main/java/com/tmall/controller/ManagerController.java
@@ -15,8 +15,16 @@ public class ManagerController {
 
     @RequestMapping("/login")
     public String login(String username , String password) {
+        // 如果密码正确，就去管理界面
+        // 这里偷懒定死了!!!
+        if ("admin".equals(username) && "admin".equals(password)) {
+            return "manager/manager";
 
-        return "manager/manager";
+        }
+
+        // 如果密码错误，就重新去登录界面
+        return "login/login";
     }
+
 
 }
